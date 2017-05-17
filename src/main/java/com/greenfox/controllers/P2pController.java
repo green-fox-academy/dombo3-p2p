172ae2fc1131bba6c8b4a0541bcf1d45809c0f1e
Dombovari Peter <dombo.peter@gmail.com>
2017-05-17 11:09:23 +0200

    create index html

---
+++ src/main/java/com/greenfox/controllers/P2pController.java
@@ -0,0 +1,13 @@
+package com.greenfox.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class P2pController {
+
+  @RequestMapping("/")
+  public String main() {
+    return "index";
+  }
+}
