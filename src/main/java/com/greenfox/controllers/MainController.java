8e3b48199c060ce24c12c46c2ce0f67bc443bbd4
Dombovari Peter <dombo.peter@gmail.com>
2017-05-20 19:35:54 +0200

    create basic layout for pages, create enter and start endpoint boilerplate

--- src/main/java/com/greenfox/controllers/MainController.java
+++ src/main/java/com/greenfox/controllers/MainController.java
@@ -5,14 +5,19 @@ import com.greenfox.repository.LogRepo;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class P2pController {
+public class MainController {
 
   @Autowired
   LogRepo logRepo;
+
+  public final String CHAT_APP_UNIQUE_ID = "dombo3";
+  public final String CHAT_APP_PEER_ADDRESS = "192.168.0.15";
 
   @RequestMapping("/")
   public String main(HttpServletRequest request) {
@@ -23,5 +28,15 @@ public class P2pController {
       logRepo.save(log);
     }
     return "index";
+  }
+
+  @GetMapping("/enter")
+  public String enter() {
+    return "enter";
+  }
+
+  @PostMapping("/enter")
+  public String saveUser() {
+    return "redirect:/";
   }
 }
