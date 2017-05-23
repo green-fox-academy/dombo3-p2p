b07c79d11e6a7a874fa5268fb1c9224312b86ebe
Dombovari Peter <dombo.peter@gmail.com>
2017-05-23 18:13:54 +0200

    add Table annotation

--- src/main/java/com/greenfox/model/User.java
+++ src/main/java/com/greenfox/model/User.java
@@ -1,13 +1,13 @@
 package com.greenfox.model;
 
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table("users")
 public class User {
 
   @Id
